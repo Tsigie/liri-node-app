843fa7e7448a54ced803de6c49ad25827f007e7f
Tsigie Kumssa <tktsigie@yahoo.com>
2017-04-13 16:59:38 -0400

    liri-node-app

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: '<3XN9tAiRIVKA7oL0WAhEdbXoe>',
+	consumer_secret: '<7alzplwd4k4pYiGnAomlajnexaRUTqxPSOCJLtJTFwtnofbNbM>',
+	access_token_key: '<2685396742-HtfUQyFl4QJGZ3GSiwuFaoSNLPb2RHPgl4ryJnD>',
+	access_token_secret: '<YD9CpXLEf0FEavEFdh10MTpZkFTr56y6mfK8BXbHJAqIr>',
+}
---
+++ liri.js
@@ -0,0 +1,24 @@
+var keys = require ("./keys.js") {
+console.log(process.argv);
+
+};
+
+var twitter = require ("twitter") {
+console.log(process.argv);
+}
+
+var spotify = require ("spotify") {
+	console.log(process.argv);
+}
+
+var request = require ("request") {
+	console.log(process.argv);
+}
+
+var fs = require ("fs") {
+	console.log(process.argv);
+}
+
+
+
+//<http://www.imdb.com/title/tt0485947/>//
